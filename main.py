d03a823e5dfadee8644fcaec75c71ada0e1da6d4
kiser00 <127670713+kiser00@users.noreply.github.com>
2026-01-22 09:32:14 +0900

    Refactor main.py to use functions for processing

--- main.py
+++ main.py
@@ -1,75 +1,74 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import function
 
-print("データを確認したいCSVファイル名を入力してください")
-file_pass = input()
+print("データを確認したいCSVファイルパスを貼り付けてください")
+file_pass = input() #自動でディレクトリ内のcsvファイルを参照したい
 
-#dfに集計したいcsv名をユーザーに指定させる
 try:
     df = pd.read_csv(file_pass)
 except FileNotFoundError:
     print("エラー: ファイルが見つかりません。")
+    exit()
 except Exception as e:
     print(f"予期せぬエラーが発生しました: {e}")
-#exit()
+    exit()
 
-columns_list = list(df.columns)
-print(columns_list)
-
-columns_list = df.columns
-print(columns_list)
+function.show_columns(df)
 
 print("計算したいカラム名を入力してください")
-columns_number_select = input("")
-if(columns_number_select in 101):
+sales_col = input("") #Series
+if(sales_col == ""):
     print("未入力です！")
-#elif(columns_number_select.swapcase() in "id"):　顧客IDをはじけるように実装
- #   print("参照するカラム名を確認してください")
+elif(sales_col == "顧客ID"):
+    print("参照するカラム名を確認してください")
 else:
-    #print(df[columns_number_select : 1])
-    de_sales = columns_number_select
-    #カンマをとって売上列を数値に変換して合計を算出
-    def convert_sales_to_int(series):
-        return (
-            series.astype(str)
-                .str.replace(",", "", regex=False)
-                .astype(int)
-        )
+    function.convert_sales_to_int(df, sales_col)
 
-    #以下df["売上"]は数値
-    df[de_sales] = convert_sales_to_int(df[de_sales])
-    #print(df[de_sales])
+    function.total_sales_sum(df, sales_col)
+    print("-------------------------------------")
 
-    total_sales = df[de_sales].sum()
+    print("日付に対応するカラム名を入力してください")
+    date_col = input("")
+    if date_col not in df.columns:
+        print("存在しないカラム名です")
+        exit()
 
-    print(f"月の合計売上：￥{total_sales}")
-
+    function.daily_sales(df, date_col, sales_col)
+    print(df[[date_col, sales_col]].head())
+    print(type(df[[date_col, sales_col]]))
     #-------------------------------------------
     print("1:日別の売上をグラフで確認する / 2:終了")
-    continue_question = int(input())
+    try:
+        continue_question = int(input())
+    except ValueError:
+        print("数値を入力してください")
+        exit()
+
 
     if(continue_question == 1):
-        print("日付に対応するカラム名を入力してください")
-        df_day = input("")
-        # 日付を日付型に
-        df[df_day] = pd.to_datetime(df[df_day])
-        # 日別合計
-        daily_sales = df.groupby(df_day)[de_sales].sum()
-        daily_sales_df = daily_sales.reset_index()
-        print(daily_sales_df)
-        #グラフ描画
-        plt.plot(daily_sales)
-        plt.title('total_sales')
-        plt.xlabel(de_sales, size=30)
-        plt.ylabel(df_day, size=30)
-        plt.grid()
-        plt.show()
+        function.plot_daily_sales(df, date_col, sales_col)
     elif(continue_question == 2):
         print("終了")
     else:
         print("1か2を選択してください")
 
+df[date_col] = pd.to_datetime(df[date_col])
+df = df.set_index(date_col)
 
-#最初に「顧客ID」、でもを入力しても動作してしまう
+weekly_avg = df[sales_col].resample("W").mean()
+weekly_total_sales = df[sales_col].resample("W").sum()
 
+total_sales_avg = weekly_avg.mean()
+vols = weekly_avg.pct_change() * 100
+
+print(f"今月の週平均売上: ￥{int(total_sales_avg)}")
+
+for i, date in enumerate(weekly_avg.index, 1):
+    total = weekly_total_sales.loc[date]
+    v = vols.loc[date]
+
+    if pd.isna(v):
+        print(f"週{i} 売上合計: ￥{int(total)}（変動率: 比較不可）")
+    else:
+        print(f"週{i} 売上合計: ￥{int(total)}（変動率: {v:.2f}%）")
